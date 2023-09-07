8bf8b7c62fbaf6f68dd8ee6ec4ca1bec64cb7019
Sara Líf Fells <saraf23@ru.is>
2023-09-07 19:32:38 +0000

    a

---
+++ bla.py
@@ -0,0 +1,11 @@
+num_int = int(input())  # Do not change this line
+# Fill in the missing code
+max_int = 0
+
+while num_int >= 0:
+    if num_int > max_int:
+        max_int = num_int
+    num_int = int(input())
+
+
+print(max_int)  # Do not change this line
